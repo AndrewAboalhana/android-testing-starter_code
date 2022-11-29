de76346f6a3ffe97ba153eb3252daaedce654f74
Androhany <hana_hany6@yahoo.com>
2022-11-30 00:06:39 +0200

    new

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -7,6 +7,7 @@ import androidx.test.espresso.matcher.ViewMatchers.isChecked
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.hamcrest.core.IsNot.not
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
@@ -16,17 +17,18 @@ import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndro
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
-import org.hamcrest.core.IsNot.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.concurrent.thread
 
+/**
+ * Integration test for the Task Details screen.
+ */
 @MediumTest
-@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
-class TaskDetailFragmentTest{
+@ExperimentalCoroutinesApi
+class TaskDetailFragmentTest {
 
     private lateinit var repository: TasksRepository
 
@@ -42,7 +44,7 @@ class TaskDetailFragmentTest{
     }
 
     @Test
-    fun activeTaskDetails_DisplayedInUi()  = runBlockingTest{
+    fun activeTaskDetails_DisplayedInUi() = runBlockingTest {
         // GIVEN - Add active (incomplete) task to the DB
         val activeTask = Task("Active Task", "AndroidX Rocks", false)
         repository.saveTask(activeTask)
@@ -50,7 +52,15 @@ class TaskDetailFragmentTest{
         // WHEN - Details fragment launched to display task
         val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
         launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
-        Thread.sleep(2000)
 
+        // THEN - Task details are displayed on the screen
+        // make sure that the title/description are both shown and correct
+        onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("Active Task")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("AndroidX Rocks")))
+        // and make sure the "active" checkbox is shown unchecked
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
     }
 }
